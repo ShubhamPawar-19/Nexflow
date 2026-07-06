2c6b429cb67b69914ceb2aaceb243c25f08eb57b
shubham <pawarshubham5959@gmail.com>
2026-07-06 23:02:06 +0530

    feat: implement Google Form trigger
    Added Google Form trigger node, webhook configuration, Apps Script generation, executor, realtime updates, and workflow execution.

---
+++ src/features/triggers/components/google-form-trigger/actions.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { googleFormTriggerChannel } from "@/inngest/channels/google-form-trigger";
+import { inngest } from "@/inngest/client";
+import { getSubscriptionToken, Realtime } from "@inngest/realtime";
+
+
+export type GoogleFormTriggerToken = Realtime.Token<typeof googleFormTriggerChannel,
+    ["status"]
+>;
+
+export async function fetchGoogleformTriggerRealtimeToken():
+    Promise<GoogleFormTriggerToken> {
+    const token = await getSubscriptionToken(inngest, {
+        channel: googleFormTriggerChannel(),
+        topics: ["status"],
+    });
+
+    return token;
+};
